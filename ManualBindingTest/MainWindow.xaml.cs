77eeb5f65cfcb35947b18468eef56fabafe982a8
Sam <sam.wheat@outlook.com>
2023-03-22 10:33:08 -0700

    Add msg

--- ManualBindingTest/MainWindow.xaml.cs
+++ ManualBindingTest/MainWindow.xaml.cs
@@ -11,9 +11,12 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Cryptography.X509Certificates;
 using System.Windows.Input;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -25,9 +28,16 @@ namespace ManualBindingTest;
 /// <summary>
 /// An empty window that can be used on its own or navigated to within a Frame.
 /// </summary>
-public sealed partial class MainWindow : Window
+public sealed partial class MainWindow : Window, INotifyPropertyChanged
 {
     public ICommand Command { get; set; }
+
+    private string _Message;
+    public string Message
+    {
+        get => _Message;
+        set => SetProp(ref _Message, value);
+    }
   
 
     public MainWindow()
@@ -37,7 +47,7 @@ public sealed partial class MainWindow : Window
         // Wire up a command handler for the existing instance of ContainerControl
         Command = new RelayCommand<object>(x =>
         {
-            int arg = Convert.ToInt32(x) + 1;
+            Message = DateTime.Now.TimeOfDay.ToString();
             
         });
 
@@ -50,5 +60,18 @@ public sealed partial class MainWindow : Window
         layoutRoot.Children.Add(cc);
     }
 
-    
+
+    #region INotifyPropertyChanged implementation
+    public event PropertyChangedEventHandler? PropertyChanged;
+    public void RaisePropertyChanged([CallerMemberNameAttribute] string propertyName = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    public void SetProp<T>(ref T prop, T value, [CallerMemberNameAttribute] string propertyName = "")
+    {
+        if (!Object.Equals(prop, value))
+        {
+            prop = value;
+            RaisePropertyChanged(propertyName);
+        }
+    }
+    #endregion    
 }
